0aa9cde534b6e8478218ea394647af0bd74d1d9a
demandred86 <noce86@gmail.com>
2023-03-29 23:00:11 +0200

    Updated Origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
 // TODO Update allowedOrigins with the actual url
-const allowedOrigins = ["http://localhost:3500"];
+const allowedOrigins = ["http://localhost:3500", "http://localhost:3001"];
 module.exports = allowedOrigins;
